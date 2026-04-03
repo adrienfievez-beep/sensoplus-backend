4f6f15d1915e5001f00347169ad85a2a7b724be0
adrienfievez-beep <adrienfievez-beep@github.com>
2026-04-03 11:56:01 +0200

    fix: drop triggers before recreating in schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -139,10 +139,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS users_updated_at ON users;
 CREATE TRIGGER users_updated_at
   BEFORE UPDATE ON users
   FOR EACH ROW EXECUTE FUNCTION update_updated_at();
 
+DROP TRIGGER IF EXISTS thresholds_updated_at ON user_thresholds;
 CREATE TRIGGER thresholds_updated_at
   BEFORE UPDATE ON user_thresholds
   FOR EACH ROW EXECUTE FUNCTION update_updated_at();
